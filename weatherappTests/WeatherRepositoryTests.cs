de5e01c75572b24a51196550c64d83dcdf074c5f
del <gsedubun@gmail.com>
2022-11-06 07:21:03 +0700

    add repository unit test.

---
+++ weatherappTests/WeatherRepositoryTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using weatherapp.Models;
+using Xunit;
+
+namespace weatherappTests
+{
+    public class WeatherRepositoryTests
+    {
+        private readonly IWeatherRepository weaterRepo;
+
+        public WeatherRepositoryTests()
+        {
+            weaterRepo = new WeatherRepository();
+        }
+        [Fact]
+        public void GetCountriesTests()
+        {
+            var c = weaterRepo.GetCountries();
+            
+            
+            Assert.NotEmpty(c);
+        }
+        
+        [Fact]
+        public void GetCities()
+        {
+            var c = weaterRepo.GetCities("indonesia");
+
+            Assert.NotEmpty(c);
+
+        }
+    }
+}
